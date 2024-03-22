6b8c7b4f1c1fba183108132ac9de53c1707dc588
d0lynnho <vinicinplays@gmail.com>
2024-03-22 11:49:42 -0300

    Comecei a fazer a fase

---
+++ Assets/Scripts/Vinicius Souza/SpawnMinerio.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMinerio : MonoBehaviour
+{
+    public Transform Minerios;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
