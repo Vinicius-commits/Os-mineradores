e2e967dd2d000f70691b66c100c70d1d3b32c923
d0lynnho <vinicinplays@gmail.com>
2024-03-25 15:04:39 -0300

    SpawnDeMinerio

--- Assets/Scripts/Vinicius Souza/SpawnMinerio.cs
+++ Assets/Scripts/Vinicius Souza/SpawnMinerio.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class SpawnMinerio : MonoBehaviour
 {
-    public Transform Minerios;
-    void Start()
+    [SerializeField] private GameObject[] minerios;
+    [SerializeField] private Vector3 Posicaotop, Posicaobot;
+    [SerializeField] private int NumMinerios;
+    public void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        for(int i = 0; i < NumMinerios; i++)
+        {
+            int j = Random.Range(0, minerios.Length);
+            Vector3 pos = new Vector3(Random.Range(Posicaotop.x, Posicaobot.x), 0.5f, Random.Range(Posicaotop.z, Posicaobot.z));
+            GameObject G = Instantiate(minerios[j], pos, Quaternion.identity);
+            G.transform.parent = transform;
+        }
     }
 }
