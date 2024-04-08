b7028b5ff65c9b681a6c2a34c6827065cc4bc74c
Corvus037 <nathanof2003@outlook.com>
2024-04-07 23:16:01 -0300

    Menu de Pausa no ESC

---
+++ Assets/Scripts/Nathan/UIScripts/Paused.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paused : MonoBehaviour
+{
+    public GameObject canvasPauseMenu; 
+    private bool isPaused = false; 
+
+    void Start()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        canvasPauseMenu.SetActive(false);
+        isPaused = false;
+    }
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+            if (isPaused)
+            {
+                Time.timeScale = 1f;
+                canvasPauseMenu.SetActive(false);
+                isPaused = false; 
+            }
+            else 
+            {
+                Time.timeScale = 0f; 
+                canvasPauseMenu.SetActive(true); 
+                isPaused = true; 
+            }
+        }
+    }
+}
