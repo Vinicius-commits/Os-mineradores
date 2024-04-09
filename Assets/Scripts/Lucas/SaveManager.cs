c8c23eb714f80d751653d4d181e879c740f3d9f2
Lucas Henrique <theplayersbp@gmail.com>
2024-04-09 16:53:19 -0300

    SaveManager

---
+++ Assets/Scripts/Lucas/SaveManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.IO;
+
+public class SaveManager : Singleton<SaveManager>
+{
+    [System.Serializable]
+    public class Museu
+    {
+        public bool FerroBool;
+        public bool OuroBool;
+        public bool AluminioBool;
+        public bool NiobioBool;
+    }
+
+    // Método para salvar dados do jogador em um arquivo JSON
+    public void SaveMuseu(Minerios minerio)
+    {
+        Museu MuseuMinerios = new Museu();
+        switch (minerio)
+        {
+            case Minerios.Ferro:
+                MuseuMinerios.FerroBool = true;
+                break;
+            case Minerios.Ouro:
+                MuseuMinerios.OuroBool = true;
+                break;
+            case Minerios.Aluminio:
+                MuseuMinerios.AluminioBool = true;
+                break;
+            case Minerios.Niobio:
+                MuseuMinerios.NiobioBool = true;
+                break;
+        }
+
+
+        string jsonData = JsonUtility.ToJson(MuseuMinerios);
+        File.WriteAllText(Application.persistentDataPath + "/Museu.json", jsonData);
+    }
+
+    // Método para carregar dados do jogador de um arquivo JSON
+    public Museu LoadMuseu()
+    {
+        string path = Application.persistentDataPath + "/Museu.json";
+        if (File.Exists(path))
+        {
+            string jsonData = File.ReadAllText(path);
+            Museu MuseuMinerios = JsonUtility.FromJson<Museu>(jsonData);
+            return MuseuMinerios;
+        }
+        else
+        {
+            Debug.LogError("ARQUIVO NAO ENCONTRADO");
+            return null;
+        }
+    }
+
+    public enum Minerios
+    {
+        Ferro,
+        Ouro,
+        Aluminio,
+        Niobio
+    }
+
+    public SaveManager()
+    {
+
+    }
+
+// Toda vez q tu der play ele carrega tudo dnv
+    public void Start() 
+    {
+        LoadMuseu();
+    }
+
+}
--- Assets/Scripts/Lucas/TerminarMinerio.cs
+++ Assets/Scripts/Lucas/TerminarMinerio.cs
@@ -7,7 +7,7 @@ public class TerminarMinerio : Interactable
     [SerializeField] private float timer;
     [SerializeField] private int contador;
     [SerializeField] private bool minerando;
-    [SerializeField] private bool ouro , ferro , aluminio , niobio;
+    [SerializeField] private bool ouro, ferro, aluminio, niobio;
     [SerializeField] private GameObject pedra;
 
     public override void Interagir()
@@ -17,7 +17,7 @@ public class TerminarMinerio : Interactable
 
     public void Mineracao()
     {
-        if(!minerando)
+        if (!minerando)
         {
             //Verifica se o player ta perto do minerio
             Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
@@ -35,17 +35,37 @@ public class TerminarMinerio : Interactable
 
     public void Quebrar() //verifica se ele ja foi minerado 3 vezes caso sim ele quebra de deixa a pedra sem minerio
     {
-        if(ouro)
+        if (ouro)
+        {
             GameManager.Instance.ouro++;
-        else if(ferro)
+            if(GameManager.Instance.ouro == 4)
+            SaveManager.Instance.SaveMuseu(SaveManager.Minerios.Ouro);
+        }
+
+        else if (ferro)
+        {
             GameManager.Instance.ferro++;
-        else if(aluminio)
+            if(GameManager.Instance.ferro == 4)
+            SaveManager.Instance.SaveMuseu(SaveManager.Minerios.Ferro);
+        }
+
+        else if (aluminio)
+        {
             GameManager.Instance.aluminio++;
-        else if(niobio)
+            if(GameManager.Instance.aluminio == 4)
+            SaveManager.Instance.SaveMuseu(SaveManager.Minerios.Aluminio);
+        }
+
+        else if (niobio)
+        {
             GameManager.Instance.niobio++;
+            if(GameManager.Instance.niobio == 4)
+            SaveManager.Instance.SaveMuseu(SaveManager.Minerios.Niobio);
+        }
+
         contador++;
         minerando = false;
-        if(contador >= 3)
+        if (contador >= 3)
         {
             pedra.SetActive(true);
             Destroy(this.gameObject);
