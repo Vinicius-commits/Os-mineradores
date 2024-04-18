a7237e0ce2eb768b21049388c23fedac6bfea086
d0lynnho <163213054+d0lynnho@users.noreply.github.com>
2024-04-18 19:38:03 -0300

    A

--- Assets/Scripts/Vinicius Souza/AtivarPainelElevador.cs
+++ Assets/Scripts/Vinicius Souza/AtivarPainelElevador.cs
@@ -6,6 +6,7 @@ public class AtivarPainelElevador : MonoBehaviour
 {
     [SerializeField] GameObject painelElevador;
 
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.F))
