c7460ea7707a227e0900a72a735bf587ec19aec7
d0lynnho <163213054+d0lynnho@users.noreply.github.com>
2024-06-14 17:12:04 -0300

    Revert "a"
    This reverts commit c485423ed27b47de74b31cc1f21fe811e2bb14e8, reversing
    changes made to 9c022070157c79ed85c83cd097808557491db56e.

--- Assets/Scripts/Lucas/SaveManager.cs
+++ Assets/Scripts/Lucas/SaveManager.cs
@@ -19,73 +19,95 @@ public class SaveManager : Singleton<SaveManager>
     public bool ManganesBool;
     public bool FluoritaBool;
 
+    // Método para salvar dados do jogador em um arquivo JSON
     public void SaveMuseu(Minerios minerio)
     {
+        Museu MuseuMinerios = new Museu();
         switch (minerio)
         {
             case Minerios.Ferro:
-                
+                MuseuMinerios.FerroBool = true;
                 FerroBool = true;
                 break;
             case Minerios.Ouro:
-                
+                MuseuMinerios.OuroBool = true;
                 OuroBool = true;
                 break;
             case Minerios.Aluminio:
-                
+                MuseuMinerios.AluminioBool = true;
                 AluminioBool = true;
                 break;
             case Minerios.Niobio:
-                
+                MuseuMinerios.NiobioBool = true;
                 NiobioBool = true;
                 break;
             case Minerios.Zinco:
-                
+                MuseuMinerios.ZincoBool = true;
                 ZincoBool = true;
                 break;
             case Minerios.Grafita:
-                
+                MuseuMinerios.GrafitaBool = true;
                 GrafitaBool = true;
                 break;
             case Minerios.Opala:
-                
+                MuseuMinerios.OpalaBool = true;
                 OpalaBool = true;
                 break;
             case Minerios.Cobre:
-                
+                MuseuMinerios.CobreBool = true;
                 CobreBool = true;
                 break;
             case Minerios.Manganes:
-               
+                MuseuMinerios.ManganesBool = true;
                 ManganesBool = true;
                 break;
             case Minerios.Magnesita:
-                
+                MuseuMinerios.MagnesitaBool = true;
                 MagnesitaBool = true;
                 break;
             case Minerios.Tungstenio:
-                
+                MuseuMinerios.TungstenioBool = true;
                 TungstenioBool = true;
                 break;
             case Minerios.Turmalina:
-                
+                MuseuMinerios.TurmalinaBool = true;
                 TurmalinaBool = true;
                 break;
             case Minerios.Uranio:
-                
+                MuseuMinerios.UranioBool = true;
                 UranioBool = true;
                 break;
             case Minerios.Gipsita:
-                
+                MuseuMinerios.GipsitaBool = true;
                 GipsitaBool = true;
                 break;
             case Minerios.Fluorita:
-                
+                MuseuMinerios.FluoritaBool = true;
                 FluoritaBool = true;
                 break;
         }
+
+
+        string jsonData = JsonUtility.ToJson(MuseuMinerios);
+        File.WriteAllText(Application.persistentDataPath + "/Museu.json", jsonData);
     }
 
+    // Método para carregar dados do jogador de um arquivo JSON
+    public Museu LoadMuseu()
+    {
+        string path = Application.persistentDataPath + "/Museu.json";
+        if (File.Exists(path))
+        {
+            string jsonData = File.ReadAllText(path);
+            Museu MuseuMinerios = JsonUtility.FromJson<Museu>(jsonData);
+            return MuseuMinerios;
+        }
+        else
+        {
+            Debug.LogError("ARQUIVO NAO ENCONTRADO");
+            return null;
+        }
+    }
 
     public enum Minerios
     {
@@ -105,7 +127,37 @@ public class SaveManager : Singleton<SaveManager>
         Opala,
         Fluorita
     }
+
+    public SaveManager()
+    {
+
+    }
+
+    // Toda vez q tu der play ele carrega tudo dnv
+    public void Start() 
+    {
+        
+        LoadMuseu();
+    }
+
 }
 
-
-
+[System.Serializable]
+    public class Museu
+    {
+        public bool FerroBool;
+        public bool OuroBool;
+        public bool AluminioBool;
+        public bool NiobioBool;
+        public bool ZincoBool;
+        public bool GrafitaBool;
+        public bool OpalaBool;
+        public bool TurmalinaBool;
+        public bool TungstenioBool;
+        public bool GipsitaBool;
+        public bool MagnesitaBool;
+        public bool ManganesBool;
+        public bool UranioBool;
+        public bool CobreBool;
+        public bool FluoritaBool;
+}
